ac54e60039b67eccb1983ade35549d3c50d2934d
Alex Hunter <almrdog@gmail.com>
2021-12-14 21:50:30 +0000

    add taxRevenue calculator

--- src/components/treasury/data/details.js
+++ src/components/treasury/data/details.js
@@ -1,5 +1,6 @@
+import { useState, useEffect } from "react";
 import { Typography, Slider, FormGroup, makeStyles } from "@material-ui/core";
-const taxRevenue = [
+const taxRevenueData = [
   { receipt: "Business rates", revenue: 16 },
   { receipt: "Corporation Tax", revenue: 30 },
   { receipt: "Council Tax", revenue: 13 },
@@ -22,7 +23,6 @@ const spending = [
   { department: "Social Security", expenditure: 102 },
   { department: "Transport", expenditure: 9 },
 ];
-const totalRevenue = taxRevenue.reduce((a, b) => a.revenue + b.revenue, 0)
 
 const useStyles = makeStyles(() => ({
   form: {
@@ -36,37 +36,58 @@ const useStyles = makeStyles(() => ({
     marginTop: "100px",
   },
   label: {
-    textAlign: "left"
+    textAlign: "left",
   },
   slider: {
     width: "80%",
   },
 }));
 export default function Form() {
-  const totalRevenue = taxRevenue.reduce((a, b) => ({revenue: a.revenue + b.revenue}))
+  const [taxRevenue, setTaxRevenue] = useState(taxRevenueData);
+  const [totalRevenue, setTotalRevenue] = useState(349);
+
+  function calculateTotalRevenue() {
+    const total = taxRevenue.reduce((a, b) => ({
+      revenue: a.revenue + b.revenue,
+    }));
+    setTotalRevenue(total.revenue);
+  }
+
+  const handleChangeSlider = (index) => (e, value) => {
+    const newArr = [...taxRevenue];
+    newArr[index].revenue = value;
+    setTaxRevenue(newArr)
+  };
+
+  useEffect(() => {
+    calculateTotalRevenue();
+  }, [taxRevenue]);
+
   const classes = useStyles();
   return (
     <div className={classes.form}>
       <FormGroup>
-        {taxRevenue.map((receipt) => {
+        {taxRevenue.map((object, index) => {
+          const {receipt, revenue} = object
           return (
-            <>
-              <Typography className={classes.label} gutterBottom>{receipt.receipt}</Typography>
+            <div key={index}>
+              <Typography className={classes.label} gutterBottom>
+                {receipt}
+              </Typography>
               <Slider
                 className={classes.slider}
                 aria-label="Temperature"
-                defaultValue={receipt.revenue}
-                // getAriaValueText={valuetext}
+                defaultValue={revenue}
+                onChange={handleChangeSlider(index)}
                 valueLabelDisplay="auto"
-                // step={10}
                 marks
                 min={0}
                 max={100}
               />
-            </>
+            </div>
           );
         })}
-        <Typography>Total: {totalRevenue.revenue}</Typography>
+        <Typography>Total: {totalRevenue}</Typography>
       </FormGroup>
     </div>
   );
